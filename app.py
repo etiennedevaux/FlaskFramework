28f5d06e065d4c497c4a6efee8428a679e6e16ca
Stephen Clarke <60775095+etiennedevaux@users.noreply.github.com>
2021-06-30 19:02:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -9,39 +9,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
-
-
-@app.route("/about")
-def about():
-    data = []
-    with open("data/company.json", "r") as json_data:
-        data = json.load(json_data)
-    return render_template("about.html", page_title="About", company=data)
-
-
-@app.route("/about/<member_name>")
-def about_member(member_name):
-    member = {}
-    with open("data/company.json", "r") as json_data:
-        data = json.load(json_data)
-        for obj in data:
-            if obj["url"] == member_name:
-                member = obj
-    return render_template("member.html", member=member)
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", page_title="Contact")
-
-
-@app.route("/careers")
-def careers():
-    return render_template("careers.html", page_title="Careers")
-
-
-if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
